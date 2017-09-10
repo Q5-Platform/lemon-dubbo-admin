c5c0dc94cf0b610dd006b4820f3d7f1a4fc194f6
lonyee <lonyee@live.com>
2017-09-11 00:03:09 +0800

    开发模板消费发送功能

--- src/main/java/cn/lemon/dubbo/admin/controller/MessageController.java
+++ src/main/java/cn/lemon/dubbo/admin/controller/MessageController.java
@@ -3,17 +3,31 @@ package cn.lemon.dubbo.admin.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.lemon.dubbo.admin.authc.RequestPermissions;
 import cn.lemon.dubbo.message.api.IMessageRecordService;
-import cn.lemon.dubbo.message.dto.MessageRecordDto;
+import cn.lemon.dubbo.message.api.IMessageService;
+import cn.lemon.dubbo.message.api.IMessageTemplateService;
+import cn.lemon.dubbo.message.dto.MessageTemplateDto;
 import cn.lemon.dubbo.system.api.IDictService;
 import cn.lemon.framework.core.BasicController;
 import cn.lemon.framework.query.Page;
@@ -24,7 +38,7 @@ import cn.lemon.framework.response.ServiceException;
 import com.alibaba.dubbo.config.annotation.Reference;
 
 /**
- * 消息模板管理接口
+ * 消息管理接口
  * @author lonyee
  */
 @Controller
@@ -34,31 +48,26 @@ public class MessageController extends BasicController {
 	@Reference
 	private IDictService dictService;
 	@Reference
+	private IMessageService messageService;
+	@Reference
 	private IMessageRecordService messageRecordService;
+	@Reference
+	private IMessageTemplateService messageTemplateService;
 	
 	@RequestMapping(value="/index", method={RequestMethod.GET})
 	public String Index(Model model) {
 		Long userId = this.getUserId();
-		model.addAttribute("messageTypes", dictService.getList(userId, "MessageType"));
-		return "message/template";
+		model.addAttribute("pushMethods", dictService.getList(userId, "PushMethod"));
+		return "message/index";
 	}
 	
-	@RequestMapping(value="/add", method={RequestMethod.GET})
+	@RequestMapping(value="/send", method={RequestMethod.GET})
 	public String add(Model model) {
 		Long userId = this.getUserId();
-		model.addAttribute("messageRecord", new MessageRecordDto());
 		model.addAttribute("messageTypes", dictService.getList(userId, "MessageType"));
-		return "message/edit";
+		return "message/send";
 	}
-	
-	@RequestMapping(value="/edit/{id}", method={RequestMethod.GET})
-	public String edit(Model model, @PathVariable("id") Long id) {
-		Long userId = this.getUserId();
-		model.addAttribute("messageRecord", messageRecordService.getById(userId, id));
-		model.addAttribute("messageTypes", dictService.getList(userId, "MessageType"));
-		return "message/edit";
-	}
-	
+		
 	@RequestMapping(value="/view/{id}", method={RequestMethod.GET})
 	public String view(Model model, @PathVariable("id") Long id) {
 		Long userId = this.getUserId();
@@ -67,7 +76,7 @@ public class MessageController extends BasicController {
 		return "message/edit";
 	}
 	
-	@ApiOperation(value="查询消息模板分页信息",notes="返回分页数据")
+	@ApiOperation(value="查询消息分页",notes="返回分页数据")
 	@ResponseBody
 	@RequestMapping(value="/pager", method={RequestMethod.GET})
 	public ResultResponse pager(@ApiParam(value="授权凭证") @CookieValue(value=TOKEN, required=true) String token, String pushMethod, String keyword, QueryPage queryPage) {
@@ -78,25 +87,49 @@ public class MessageController extends BasicController {
 		return resultResponse.success(page);
 	}
 	
-	@ApiOperation(value="添加或修改消息模板信息",notes="返回success")
+	
+	
+	@ApiOperation(value="获取参数列表信息",notes="返回数据")
 	@ResponseBody
-	@RequestMapping(value="/save", method={RequestMethod.POST})
-	public ResultResponse save(@ApiParam(value="授权凭证") @CookieValue(value=TOKEN, required=true) String token, MessageRecordDto messageRecordDto) throws ServiceException {
+	@RequestMapping(value="/params", method={RequestMethod.GET})
+	public ResultResponse send(@ApiParam(value="授权凭证") @CookieValue(value=TOKEN, required=true) String token, String messageType) throws ServiceException {
 		Long userId = this.getUserId();
-		if (messageRecordDto.getId()==null || messageRecordDto.getId()==0) {
-			//messageRecordService.save(userId, messageRecordDto);
-		} else {
-			//messageRecordService.update(userId, messageRecordDto);
+		List<MessageTemplateDto> listDto = messageTemplateService.getListByMessageType(userId, messageType);
+		List<String> list = new ArrayList<String>(10);
+		for (MessageTemplateDto messageTemplateDto: listDto) {
+			Pattern pattern = Pattern.compile("(?<=\\$\\{)(.+?)(?=\\})");
+			Matcher matcher = pattern.matcher(messageTemplateDto.getTitle() + messageTemplateDto.getTemplate());
+			while(matcher.find()){
+				String param = matcher.group();
+				//排除默认参数
+				if (!"messageType".equals(param) && !"receiverId".equals(param)&& !"receiver".equals(param)&& !"scheduleTime".equals(param)) {
+				    list.add(param);
+				}
+			}
 		}
+		return resultResponse.success(list);
+	}
+	
+	@ApiOperation(value="发送信息",notes="返回success")
+	@ResponseBody
+	@RequestMapping(value="/send", method={RequestMethod.POST})
+	public ResultResponse send(@ApiParam(value="授权凭证") @CookieValue(value=TOKEN, required=true) String token, String messageType, String receiver, Long receiverId, Date scheduleTime, @RequestParam Map<String, String> params) throws ServiceException {
+		Long userId = this.getUserId();
+		messageService.sendMessage(userId, messageType, receiver, receiverId, scheduleTime, params);
 		return resultResponse.success();
 	}
 	
-	@ApiOperation(value="删除消息模板信息",notes="返回success")
+	@ApiOperation(value="删除消息",notes="返回success")
 	@ResponseBody
 	@RequestMapping(value="/delete/{id}", method={RequestMethod.POST})
 	public ResultResponse delete(@ApiParam(value="授权凭证") @CookieValue(value=TOKEN, required=true) String token, @PathVariable("id") Long id) throws ServiceException {
 		Long userId = this.getUserId();
 		messageRecordService.delete(userId, id);
 		return resultResponse.success();
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+	    binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
 	}
 }
